aff2bc8bccbe67073c244b065368a7e2186b33ed
GoldeneyeRohan <raghav_anand98@yahoo.com>
2018-02-24 17:26:51 +0000

    done

---
+++ custom_workspace/src/goldeneye/src/data_record/data_recorder.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import subprocess
+import os
+import datetime
+
+DATA_TOPICS = ['camera', 'gps', 'actuation', 'lidar', 'imu', 'encoders']
+
+class DataCollector():
+    def __init__(self, params):
+        self.topic_mappings = {
+            'camera': '/image',
+            'gps': 'TODO',
+            'actuation': '/ecu_pwm',
+            'lidar': 'TODO',
+            'imu': 'TODO',
+            'encoders': 'TODO'
+        }
+        self.root_path = params['root_path']
+        self.topic_booleans = {topic: params[topic] for topic in DATA_TOPICS}
+        self.special_topics = ['camera']
+        self.init_log_file()
+
+    def get_timestamp(self):
+        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def init_log_file(self):
+        self.start_time = self.get_timestamp()
+        self.log_dir = os.path.join(self.root_path, self.start_time)
+        os.makedirs(self.log_dir)
+        log_file_path = os.path.join(self.log_dir, 'log')
+        log_file = open(log_file_path, 'w+')
+        log_file.write(self.start_time)
+        log_file.write(self.topic_booleans.__str__())
+        log_file.close()
+
+    def record(self):
+        rosbag_command = 'rosbag record '
+        for topic, rec in self.topic_booleans.items():
+            if rec and topic not in self.special_topics:
+                rosbag_command += self.topic_mappings[topic]
+        print('RUNNING: ', rosbag_command)
+        self.rosbag_proc = subprocess.Popen(rosbag_command, stdin=subprocess.PIPE, shell=True, cwd=self.log_dir)
+
+        if self.topic_booleans['camera']:
+            camera_command = 'rosrun image_view video_recorder image:=/image_raw _fps:=30 _image_transport:=compressed'
+            print('RUNNING: ', camera_command)
+            self.video_proc = subprocess.Popen(camera_command, stdin=subprocess.PIPE, shell=True, cwd=self.log_dir)
+
+    def end_all(self):
+        self.rosbag_proc.kill()
+        if self.topic_booleans['camera']: self.camera_command.kill()
+
+def data_recorder():
+    param_names = DATA_TOPICS + ['root_path']
+    params = {}
+    for param in param_names:
+        params[param] = rospy.get_param(param)
+    data_collector = DataCollector(params)
+    data_collector.record()
+    rospy.on_shutdown(data_collector.end_all)
+    rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('data_collector')
+    try: 
+        data_recorder()
+    except rospy.ROSInterruptException:
+        pass
--- custom_workspace/src/goldeneye/src/lane_keeper/simple_lane_follower.py
+++ custom_workspace/src/goldeneye/src/lane_keeper/simple_lane_follower.py
@@ -9,21 +9,24 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 
-#out = cv2.VideoWriter('output3.avi', cv2.VideoWriter_fourcc(*'PIM1'), 20.0, ( 1288, 964), False)
 
-def image_process(image):
+def image_process(image, params):
     bridge = CvBridge()
+    lower = params['lowerY']
+    upper = params['upperY']
+    debug_info = params['debug_info']
     global error
     try:
         cv_image = bridge.imgmsg_to_cv2(image)
-        #out.write(cv_image)
         gray = cv_image
-        #gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         _, gray = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)
-        gray = gray[200:600, :]
+        y_len, x_len = gray.shape
+        lower, upper = max(0, lower), min(upper, y_len)
+        if debug_info: print(lower, upper, 'image crop in Y')
+        gray = gray[lower:upper, :]
         y_len, x_len = gray.shape
         total_error = []
-        for y in range(10, y_len,90):
+        for y in range(10, y_len, 90):
             weighted_mass = 0
             mass = 0
             for x in range(0, x_len):
@@ -33,61 +36,33 @@ def image_process(image):
             if mass > 0:
                 final_x = int(weighted_mass / mass)
                 total_error.append(float(final_x - x_len // 2) / float(x_len // 2))
-                #cv2.rectangle(gray, (final_x-10,y), (final_x + 10, y+35), 120, 2)
+                if params['display_processed_image']: cv2.rectangle(gray, (final_x-10,y), (final_x + 10, y+35), 120, 2)
         #cv2.imshow('wooho', gray)
 
-        #print(total_error)
+        if params['debug_info']: print(total_error)
         if len(total_error) > 2:
             error = sum(total_error) / len(total_error)
-        else:
-            pass
-            #error = 0
-
-
-        #print(error)
     
-        #cv2.imshow('full image', cv_image[0:midx+100,:])
-        #cv2.imshow('bottom half',cv_bottom)
+        if params['display_image']: cv2.imshow('raw', cv_image)
+        if params['display_processed_image']: cv2.imshow('processed', gray)
         cv2.waitKey(1)
         
-        #cols = []
-        #x,y = cv_image.shape
-        ##print(x,y)
-        ##print(type(cv_image))
-        #img = np.array(cv_image)
-        #midx = int(round(x/2))
-        #midy = int(round(y/2))
-        ##cv_bottom = cv_image[midx:,:]
-        #
-        #cols = np.zeros(y)
-        #cv_image = cv2.Canny(cv_image, 250,255)
-        #for i in range(0,midx+100,15):
-        #    for j in range(0,y,1):
-        #        if cv_image[i,j] == 255:
-        #            cols[j] += 1
-
-        #
-        #npix = 0
-        #tfrac = 0
-        #for k in range(0,y-1):
-        #    tfrac += k*cols[k]
-        #    npix += cols[k]
-
-        #if npix == 0:
-        #    avgcol = midy
-        #else:
-        #    avgcol = tfrac/npix
-        #print(avgcol)
-        #print(midy)
     except CvBridgeError as e:
-        print(e)
+        if params['debug_info']:
+            print(e)
 
 def image_node():
     global error
     error = 0
 
+    param_names = ['lowerY', 'upperY', 'display_image', 'display_processed_image', 'debug_info']
+    params = {}
+    for param in param_names:
+        params[param] = rospy.get_param(param)
+    print(params)
+
     rospy.init_node('Test_Imager')
-    rospy.Subscriber('image_raw', Image, image_process)
+    rospy.Subscriber('image_raw', Image, image_process, params)
     pub = rospy.Publisher('ecu_pwm', ECU, queue_size=10)
     rate = rospy.Rate(10)
     
@@ -96,7 +71,9 @@ def image_node():
     integral = 0
     while not rospy.is_shutdown():
         msg = ECU()
-        print(error)
+
+        if params['debug_info']:
+            print(error)
         msg.motor = 1590
         msg.servo = 1500 + K*error # Ki*integral
         pub.publish(msg)
